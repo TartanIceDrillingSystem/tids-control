361a3dda7d3ead1b22f85f55154721a25c11525b
Devin Gund <devin@dgund.com>
2018-05-15 14:06:54 -0400

    Add driver for HX711 load cell amplifier

---
+++ src/HX711.cpp
@@ -0,0 +1,150 @@
+/*
+    Tartan Ice Drilling System (TIDS) for autonomous martian ice extraction.
+    Copyright (C) 2018 Devin Gund (https://dgund.com)
+
+    Driver for the HX711 Load Cell Amplifier
+    http://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "HX711.h"
+
+#include <cstdint>
+
+// Length of HX711 data to read, in bits
+#define HX711_DATA_LENGTH 24
+
+namespace tids {
+
+HX711::HX711(bbbkit::GPIO::PIN pinDOUT, bbbkit::GPIO::PIN pinPD_SCK, GAIN gain=A_128, long offset=0, float scale=1.0f) {
+    this->pinDOUT = new bbbkit::GPIO(pinDOUT, bbbkit::GPIO::DIRECTION::INPUT);
+    this->pinPD_SCK = new bbbkit::GPIO(pinPD_SCK, bbbkit::GPIO::DIRECTION::OUTPUT);
+    this->gain = gain;
+    this->offset = offset;
+    this->scale = scale;
+}
+
+HX711::~HX711() {
+    delete this->pinDOUT;
+    delete this->pinPD_SCK;
+}
+
+// Check if HX711 has data ready to read
+bool HX711::isReady() {
+    return this->pinDOUT->getValue() == bbbkit::GPIO::VALUE::LOW;
+}
+
+// Get gain factor
+GAIN HX711::getGain() {
+    return this->gain;
+}
+
+// Set new gain factor after the next read
+int HX711::setGain(GAIN gain) {
+    this->gain = gain;
+    // Perform a read in order to set the new gain factor
+    this->readRaw();
+}
+
+// Read raw data value
+long HX711::readRaw() {
+    uint32_t data = 0;
+    for (int bitIndex = 0; bitIndex < HX711_DATA_LENGTH; bitIndex++) {
+        // Read data bit by switching clock pin
+        this->pinPD_SCK->setValue(bbbkit::GPIO::VALUE::HIGH);
+        unsigned int bit = static_cast<unsigned int>(this->pinDOUT->getValue());
+        this->pinPD_SCK->setValue(bbbkit::GPIO::VALUE::LOW);
+
+        // Construct 24-bit data
+        data |= (bit << (HX711_DATA_LENGTH - 1 - bitIndex));
+    }
+
+    // Write the gain by switching clock pin
+    for (int gainIndex = 0; gainIndex < static_cast<int>(this->gain); gainIndex++) {
+        this->pinPD_SCK->setValue(bbbkit::GPIO::VALUE::HIGH);
+        this->pinPD_SCK->setValue(bbbkit::GPIO::VALUE::LOW);
+    }
+
+    // Pad 32-bits for signed data
+    if (data & (0x1 << (HX711_DATA_LENGTH - 1))) {
+        data |= ((uint32_t)0xFF << HX711_DATA_LENGTH);
+    }
+
+    // Cast from uint32 to int32 to long in order to preserve signed value for any size of long
+    return static_cast<long>(static_cast<int32_t>(data));
+}
+
+// Read raw data value (averaged over count)
+long HX711::readRaw(int count) {
+    if (count < 1) {
+        return 0;
+    }
+
+    // Sum raw data
+    long long readSum = 0;
+    for (int i = 0; i < count; i++) {
+        long data = this->readRaw();
+        readSum += data;
+    }
+
+    // Average over count
+    return static_cast<long>(readSum / count);
+}
+
+// Read weight value for the specified offset and scale (averaged over count)
+float HX711::readWeight(int count) {
+    return (this->readRaw() - this->offset) / this->getScale();
+}
+
+// Get offset
+long HX711::getOffset() {
+    return this->offset;
+}
+
+// Set offset
+int HX711::setOffset(long offset) {
+    this->offset = offset;
+}
+
+// Get scale
+float HX711::getScale() {
+    return this->scale;
+}
+
+// Set scale
+int HX711::setScale(float scale) {
+    this->scale = scale;
+}
+
+// Zero HX711 by setting the current weight as the offset (averaged over count)
+int HX711::tare(int count) {
+    long rawAvg = readRaw(count);
+    this->setOffset(rawAvg);
+}
+
+// Power down HX711
+void HX711::powerDown() {
+    // Hold clock pin high
+    this->pinPD_SCK->setValue(bbbkit::GPIO::VALUE:::LOW);
+    this->pinPD_SCK->setValue(bbbkit::GPIO::VALUE::HIGH);
+}
+
+// Wake up HX711
+void HX711::powerUp() {
+    // Reset clock pin low
+    this->pinPD_SCK->setValue(bbbkit::GPIO::VALUE::LOW);
+}
+
+} /* namespace tids */
---
+++ src/HX711.h
@@ -0,0 +1,88 @@
+/*
+    Tartan Ice Drilling System (TIDS) for autonomous martian ice extraction.
+    Copyright (C) 2018 Devin Gund (https://dgund.com)
+
+    Driver for the HX711 Load Cell Amplifier
+    http://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HX711_H
+#define HX711_H
+
+#include <libbbbkit/GPIO.h>
+
+namespace tids {
+
+class HX711 {
+public:
+    enum GAIN {
+        A_128 = 1,
+        B_32 = 2,
+        A_64 = 3,
+    };
+private:
+    bbbkit::GPIO *gpioDOUT;
+    bbbkit::GPIO *gpioPD_SCK;
+    GAIN gain;
+    long offset;
+    float scale;
+public:
+    HX711(bbbkit::GPIO::PIN pinDOUT, bbbkit::GPIO::PIN pinPD_SCK, GAIN gain=A_128, long offset=0, float scale=1.0f);
+    virtual ~HX711();
+
+    // Check if HX711 has data ready to read
+    bool isReady();
+
+    // Get gain factor
+    int getGain();
+
+    // Set new gain factor after the next read
+    int setGain(GAIN gain);
+
+    // Read raw data value
+    long readRaw();
+
+    // Read raw data value (averaged over count)
+    long readRaw(int count);
+
+    // Read weight value for the specified offset and scale (averaged over count)
+    float readWeight(int count=1);
+
+    // Get offset
+    long getOffset();
+
+    // Set offset
+    int setOffset(long offset);
+
+    // Get scale
+    float getScale();
+
+    // Set scale
+    int setScale(float scale);
+
+    // Zero HX711 by setting the current weight as the offset (averaged over count)
+    int tare(int count=1);
+
+    // Power down HX711
+    void powerDown();
+
+    // Wake up HX711
+    void powerUp();
+}
+
+} /* namespace tids */
+
+#endif /* HX711_H */
